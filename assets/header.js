61a4c07ca5af0bff6897e7525d0bb6b9672ca863
Kambatukebele <kambacharles11@gmail.com>
2025-03-20 12:20:55 +0100

    added the script tag in the theme.liquid with it link to header.js

---
+++ assets/header.js
@@ -0,0 +1,38 @@
+// Toggle menu on mobile
+const ToggleMenu = () => {
+  const hamburgerMenu = document.querySelector("#hamburgerMenu");
+  const closeMenu = document.querySelector("#closeMenu");
+  const menu = document.querySelector("#menu");
+
+  hamburgerMenu.addEventListener("click", () => {
+    menu.classList.remove("-left-[1300px]");
+    menu.classList.add("left-0");
+    closeMenu.classList.remove("hidden");
+    hamburgerMenu.classList.add("hidden");
+  });
+
+  closeMenu.addEventListener("click", () => {
+    menu.classList.add("-left-[1300px]");
+    menu.classList.remove("left-0");
+    closeMenu.classList.add("hidden");
+    hamburgerMenu.classList.remove("hidden");
+  });
+};
+
+const ToggleSearch = () => {
+  const searchBox = document.querySelector("#search");
+  const searchIcon = document.querySelector("#searchIcon");
+  const closeSearch = document.querySelector("#closeSearch");
+
+  searchIcon.addEventListener("click", () => {
+    searchBox.classList.remove("lg:-top-96");
+    searchBox.classList.add("lg:top-0");
+  });
+  closeSearch.addEventListener("click", () => {
+    searchBox.classList.add("lg:-top-96");
+    searchBox.classList.remove("lg:top-0");
+  });
+};
+
+ToggleMenu();
+ToggleSearch();
