2726d0dbe5e33d780f77eb5ea0cd3c1d41f0fa10
Kambatukebele <kambacharles11@gmail.com>
2025-04-07 11:38:04 +0200

    added the possibilty to see icons card on smalll device

--- assets/product-card.js
+++ assets/product-card.js
@@ -5,17 +5,17 @@ productCards.forEach((productCard, index) => {
   productIcons.forEach((productIcon, k_index) => {
     if (index === k_index) {
       productCard.addEventListener("mouseenter", () => {
-        productIcon.classList.remove("opacity-0");
-        productIcon.classList.add("opacity-100");
-        productIcon.classList.remove("-right-5");
-        productIcon.classList.add("right-2");
+        productIcon.classList.remove("lg:opacity-0");
+        productIcon.classList.add("lg:opacity-100");
+        productIcon.classList.remove("-lg:right-5");
+        productIcon.classList.add("lg:right-2");
       });
     }
     productCard.addEventListener("mouseleave", () => {
-      productIcon.classList.remove("opacity-100");
-      productIcon.classList.add("opacity-0");
-      productIcon.classList.remove("right-2");
-      productIcon.classList.add("-right-5");
+      productIcon.classList.remove("lg:opacity-100");
+      productIcon.classList.add("lg:opacity-0");
+      productIcon.classList.remove("lg:right-2");
+      productIcon.classList.add("-lg:right-5");
     });
   });
 });
