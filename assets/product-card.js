2393ff944799bd7666d8aa828aa193c4e67b5257
Kambatukebele <kambacharles11@gmail.com>
2025-04-08 12:50:32 +0200

    working on show quick view product on click

--- assets/product-card.js
+++ assets/product-card.js
@@ -1,5 +1,7 @@
 const productCards = document.querySelectorAll(".productCard");
 const productIcons = document.querySelectorAll(".productIcon");
+const quickViewProduct = document.querySelector("#quick-view-product");
+const closeQuickViewProduct = document.querySelector("#closeQuickViewProduct");
 
 productCards.forEach((productCard, index) => {
   productIcons.forEach((productIcon, k_index) => {
@@ -10,6 +12,15 @@ productCards.forEach((productCard, index) => {
         productIcon.classList.remove("-lg:right-5");
         productIcon.classList.add("lg:right-2");
       });
+
+      // Show Quic product on click
+      productIcon.addEventListener("click", () =>{        
+        quickViewProduct.classList.replace("hidden", "block");        
+      });
+      //hide quick product on click
+      closeQuickViewProduct.addEventListener("click", () =>{
+        quickViewProduct.classList.replace("block", "hidden");
+      })
     }
     productCard.addEventListener("mouseleave", () => {
       productIcon.classList.remove("lg:opacity-100");
