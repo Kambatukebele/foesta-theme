15eb060df6414b4517c1df759a73f9e5706b4969
Kambatukebele <kambacharles11@gmail.com>
2025-04-24 10:59:57 +0200

    updated the menu by adding multiple levels (submenu)

--- assets/header.js
+++ assets/header.js
@@ -3,21 +3,84 @@ const ToggleMenu = () => {
   const hamburgerMenu = document.querySelector("#hamburgerMenu");
   const closeMenu = document.querySelector("#closeMenu");
   const menu = document.querySelector("#menu");
-
+  // Targeting the level menu
   hamburgerMenu.addEventListener("click", () => {
     menu.classList.remove("-left-[1300px]");
     menu.classList.add("left-0");
     closeMenu.classList.remove("hidden");
     hamburgerMenu.classList.add("hidden");
-  });
-
-  closeMenu.addEventListener("click", () => {
+    // Closing
+    closeMenu.addEventListener("click", () => {
     menu.classList.add("-left-[1300px]");
     menu.classList.remove("left-0");
     closeMenu.classList.add("hidden");
     hamburgerMenu.classList.remove("hidden");
+    });
   });
-};
+  // Toggling submenu
+  function toggleSubMenu (){
+    const plusMenus = document.querySelectorAll(".plusMenus"); // targeting the plus on the main menu
+    const minusMenus = document.querySelectorAll(".minusMenus"); // targeting the minus on the main menu
+    const subMenuLists = document.querySelectorAll(".subMenuLists"); // targeting the submenu lists, first level
+    const plusSubMenus = document.querySelectorAll(".plusSubMenus"); // targeting the plus on the first level menu
+    const minusSubMenus = document.querySelectorAll(".minusSubMenus"); // targeting the minus on the first level menu
+    const secondLevelSubMenuLists = document.querySelectorAll(".secondLevelSubMenuLists"); // targeting the second level submenu lists
+
+    // Show the first submenu lists
+    plusMenus.forEach((plus, index) =>{
+      minusMenus.forEach((minus, min_index) =>{
+        subMenuLists.forEach((subMenuList, sub_index) =>{
+          plus.addEventListener("click", () =>{
+            //console.log("click on index " + index + " " + subMenuLists);
+            if (index === sub_index) {
+              subMenuList.classList.replace("hidden", "block");
+            }
+            if (index === min_index) {
+              minus.classList.replace("hidden", "block");
+              plus.classList.replace("block", "hidden");
+            }
+          });
+          minus.addEventListener("click", ()=>{
+            if (index === sub_index) {
+              subMenuList.classList.replace("block", "hidden");
+            }  
+            if (index === min_index) {
+              minus.classList.replace("block", "hidden");
+              plus.classList.replace("hidden", "block");
+            }          
+          });
+        })
+      }) 
+    })    
+    // Loop the subMenus
+    plusSubMenus.forEach((plusSubMenu, plusSub_index) =>{
+      minusSubMenus.forEach((minusSubMenu, minusSub_index) =>{
+        secondLevelSubMenuLists.forEach((secondLevelSubMenuList, secondLevelSub_index) =>{
+          plusSubMenu.addEventListener("click", ()=>{
+            if (plusSub_index === secondLevelSub_index) {
+              secondLevelSubMenuList.classList.replace("hidden", "block");
+            }
+            if (plusSub_index === minusSub_index){
+              plusSubMenu.classList.replace("block", "hidden");
+              minusSubMenu.classList.replace("hidden", "block");
+            }         
+          });
+          minusSubMenu.addEventListener("click", ()=>{
+            if (plusSub_index === secondLevelSub_index) {
+              secondLevelSubMenuList.classList.replace("block", "hidden");
+            }
+            if (plusSub_index === minusSub_index){
+              plusSubMenu.classList.replace("hidden", "block");
+              minusSubMenu.classList.replace("block", "hidden");
+            }      
+          })
+        });
+      });
+    });
+  
+  }
+  toggleSubMenu();
+}
 // Toggle search bar
 const ToggleSearch = () => {
   const searchBox = document.querySelector("#search");
